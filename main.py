2672f5d82cfee102284b1886f23aa642ab555379
RandyVald <radionburda@gmail.com>
2024-04-21 23:44:55 +0300

    рабочая версия

--- main.py
+++ main.py
@@ -1,39 +1,39 @@
 import pandas as pd
 import random
-# import seaborn as sns
-# import matplotlib.pyplot as plt
 
+def get_one_hot(data):
+    name_col = data.columns[0] 
+    set_1 = set()
+    list_1 = list()
+   
+    for name, values in data.iterrows():
+        current_value = values[name_col]
+        set_1.add(current_value)
+        list_1.append(current_value)
 
-# print('hello')
+    set_1 = sorted(set_1)
 
+    for i in set_1:
+        list_next = list() 
+        for j in list_1:
+            res = i == j
+            list_next.append(res)
+        data[i] = list_next
+    data.drop(name_col,axis=1,inplace=True)    
 
 lst = ['robot'] * 10
-# print(lst)
 lst += ['human'] * 10
-# print(lst)
 random.shuffle(lst)
-# print(lst)
 data = pd.DataFrame({'whoAmI':lst})
 
-# list_robot = list()
-set_u = set()
-for i in lst:
-    set_u.add(i)
+print('Исходные данные:')
+print(data)
+data_test_of_get_dumnies = pd.get_dummies(data['whoAmI'])
 
-# print(set_u)  
-for i in set_u:
-    list_next = list() 
-    for j in lst:
-        res = i ==j
-        list_next.append(res)
-    data[i] = list_next
-    print(list_next)    
-# print(list_robot)
-
-# data['robot'] = 
-
+print('Данные в виде one hot:')
+get_one_hot(data)
 print(data)
 
-# print(data.head(10))
-# print(pd.get_dummies(data['whoAmI']))
+# Проверка результата
+print(data.equals(data_test_of_get_dumnies))
 
